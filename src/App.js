25708c67a5a288be9d482887f4a12a4b37e6fcd2
MailenSz <mailenszpyrka@gmail.com>
2022-11-17 22:25:31 -0300

    instalacion y configuracion del entorno

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Title from './Components/Title';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Title />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          HALAWAI BAR and GRILL
         </p>
         <a
           className="App-link"
